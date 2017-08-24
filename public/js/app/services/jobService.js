a05926afd047e29dddf8f30d46f473f09f72eff9
Daniel Pleshkov <dplesh255@gmail.com>
2017-08-24 21:10:12 +0300

    Add working basic client job MVC

--- public/js/app/services/jobService.js
+++ public/js/app/services/jobService.js
@@ -2,19 +2,13 @@
 
 angular.module('mainApp')
 .service('jobService', ['$resource', function($resource) {
-    let Job = $resource('/job/:jobId', {jobId:'@id'});
-    let UserJob = $resource('/user/job/:userJobId', {userJobid : '@id'});
+    let Job = $resource('/jobs/:jobId', {jobId:'@id'});
+
+    //let UserJob = $resource('/user/jobs/:userJobId', {userJobid : '@id'});
     
     return {
-        // gets all user jobs, by session context
-        getAllUserJobs : function() {
-            return UserJob.get().$promise;
-        },
-        getUserJobById : function(userJobId) {
-            return UserJob.get({userJobid : userJobId}).$promise;
-        },
         getAllJobs : function() {
-            return Job.get().$promise;
+            return Job.query().$promise;
         },
         getJobById : function(jobId) {
           return Job.get({jobId : jobId}).$promise;
