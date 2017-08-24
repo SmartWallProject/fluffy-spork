6b5f21997924730455723e1335039447b6701791
Daniel Pleshkov <dplesh255@gmail.com>
2017-08-25 01:26:33 +0300

    Add jobService get tasks for certain job

--- public/js/app/controllers/jobController.js
+++ public/js/app/controllers/jobController.js
@@ -1,15 +1,21 @@
 'use strict';
 
 angular.module('mainApp')
-.controller('jobController', ['jobService', '$routeParams', function(jobService, $routeParams) {
-    let self = this;
-    
-    jobService.getJobById($routeParams.jobId)
-    .then(function(res) {
-        console.log('Received reponse: ' + res);
-        self.job = res;
-    })
-    .catch(function(res) {
-        console.error('Received reponse: ' + res);
-    })
-}])
+    .controller('jobController', ['jobService', '$routeParams', function (jobService, $routeParams) {
+        let self = this;
+        let jobId = $routeParams.jobId;
+        jobService.getJobById(jobId)
+            .then(function (res) {
+                console.log('Received reponse: ' + res);
+                self.job = res;
+            })
+            .then(function () {
+                return jobService.getJobById(jobId);
+            })
+            .then(function (res) {
+                self.tasks = res;
+            })
+            .catch(function (res) {
+                console.error('Received reponse: ' + res);
+            })
+    }])
