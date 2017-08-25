30b4f4f22b1f90a649826632c946ad78f12acf02
Daniel Pleshkov <dplesh255@gmail.com>
2017-08-25 04:38:13 +0300

    Ugprage homepage for goodness

--- public/js/app/controllers/homeController.js
+++ public/js/app/controllers/homeController.js
@@ -5,6 +5,7 @@ angular.module('mainApp')
         let self = this;
         this.username = "X";
         this.pendingTasks = [];
+        this.jobList = [];
 
         profileService.getCurrentUserProfile()
             .then(function (res) {
@@ -14,7 +15,6 @@ angular.module('mainApp')
             .catch(function (res) {
                 console.log("Error retreiving user profile: " + res);
             })
-
         let setPendingTasks = function () {
             return jobService.getMyJobs()
                 .then(function(res) {
@@ -32,6 +32,36 @@ angular.module('mainApp')
                 })
 
         }
+        let setJobList = function() {
+            return jobService.getMyJobs()
+            .then(function(res){
+                let jobList = res;
+                let placeholderIndex = 0;
+                if (jobList.length > 3){
+                    jobList = jobList.splice(0,3);
+                }
+                else {
+                    while (jobList.length < 3){
+                        if (placeholderIndex == 0){
+                            jobList.push({description: "Explore and assign for new Jobs"});
+                        }
+                        if (placeholderIndex == 1){
+                            jobList.push({description: "Complete jobs to earn LFTs"});
+                        }
+                        if (placeholderIndex == 2) {
+                            jobList.push({description: "Buy goodies using your LFTs"});
+                        }
+                        placeholderIndex++;
+                    }
+                }
+                self.jobList = jobList;
+
+            })
+            .catch(function(res) {
+                console.error("Somthing went wrong requesting jobs.")
+            })
+        };
         setPendingTasks();
+        setJobList();
 
     }])
